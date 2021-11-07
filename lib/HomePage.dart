989b8fccc5395f2bc0de30d9a4f8b7f821880f21
milushyanev <milushyanev@yahoo.com>
2021-11-06 23:07:31 -0700

    adding functionalities

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,6 +13,11 @@ class _HomePageState extends State<HomePage> {
   double interestRate =2.0;
   double monthlyIncome =1000;
   double expenses = 0;
+  double downPayment=0;
+  double propertyTax=0.5;
+  double hoaTax=0;
+  double homeInsurance=0;
+  double mortgageInsurance=0;
 
   loanCalculation(){
     setState(() {
@@ -62,61 +67,7 @@ class _HomePageState extends State<HomePage> {
             child:ListView(
              children: <Widget>[
                Padding(padding: EdgeInsets.all(30)),
-              Container(
-                margin: const EdgeInsets.only(left: 30.0, right: 30.0),
-                decoration: BoxDecoration(
 
-                    border: Border.all(width:1,color: Colors.white),
-                    borderRadius: const BorderRadius.all(const Radius.circular(8)),
-                    color: Colors.white.withOpacity(.3)
-                ),
-                child:Column(
-                  children: <Widget>[
-                    Slider(
-                        min:100000,
-                        max:1000000,
-                        divisions: 180,
-                        label: loanAmount.round().toString(),
-                        onChanged: (housePrice){
-                          setState((
-                              )  {
-                            loanAmount = housePrice;
-                          });
-                        },
-                        value: loanAmount ),
-                    Container(
-                     margin: const EdgeInsets.only(left: 10.0, right: 10.0),
-                     child: Padding(
-                       padding: const EdgeInsets.all(10.0),
-                       child: Row(
-                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                         children: <Widget>[
-
-                           Text('Home Value', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
-                               shadows: [
-                                 Shadow(
-                                     color: Colors.blue,
-                                     offset: Offset(1, 2),
-                                     blurRadius: 1),
-                               ]),
-                           ),
-                           Text('\$ ${double.parse(loanAmount.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
-                               shadows: [
-                                 Shadow(
-                                     color: Colors.blue,
-                                     offset: Offset(2, 2),
-                                     blurRadius: 1),
-                               ]),
-                           )
-                         ],
-                       ),
-                     ),
-                    )
-                  ],
-
-                ),
-              ),
-               Padding(padding: EdgeInsets.all(20)),
                Container(
                  margin: const EdgeInsets.only(left: 30.0, right: 30.0),
                  decoration: BoxDecoration(
@@ -170,6 +121,171 @@ class _HomePageState extends State<HomePage> {
 
                  ),
                ),
+               Padding(padding: EdgeInsets.all(20)),
+
+               Container(
+                 margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                 decoration: BoxDecoration(
+                     border: Border.all(width:1,color: Colors.white),
+                     borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                     color: Colors.white.withOpacity(.3)
+                 ),
+                 child:Column(
+                   children: <Widget>[
+                     Slider(
+                         min:0,
+                         max:10000,
+                         divisions: 400,
+                         label: expenses.round().toString(),
+                         onChanged: (monthlyExpenses){
+                           setState((
+                               )  {
+                             expenses = monthlyExpenses;
+                           });
+                         },
+                         value: expenses ),
+                     Container(
+                       margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                       child: Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: <Widget>[
+
+                             Text('Monthly Expenses', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(1, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             ),
+                             Text('\$ ${double.parse(expenses.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(2, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             )
+                           ],
+                         ),
+                       ),
+                     )
+                   ],
+
+                 ),
+               ),
+               Padding(padding: EdgeInsets.all(20)),
+               Container(
+                 margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                 decoration: BoxDecoration(
+
+                     border: Border.all(width:1,color: Colors.white),
+                     borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                     color: Colors.white.withOpacity(.3)
+                 ),
+                 child:Column(
+                   children: <Widget>[
+                     Slider(
+                         min:100000,
+                         max:1000000,
+                         divisions: 180,
+                         label: loanAmount.round().toString(),
+                         onChanged: (housePrice){
+                           setState((
+                               )  {
+                             loanAmount = housePrice;
+                           });
+                         },
+                         value: loanAmount ),
+                     Container(
+                       margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                       child: Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: <Widget>[
+
+                             Text('Home Value', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(1, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             ),
+                             Text('\$ ${double.parse(loanAmount.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(2, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             )
+                           ],
+                         ),
+                       ),
+                     )
+                   ],
+
+                 ),
+               ),
+               Padding(padding: EdgeInsets.all(20)),
+               Container(
+                 margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                 decoration: BoxDecoration(
+                     border: Border.all(width:1,color: Colors.white),
+                     borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                     color: Colors.white.withOpacity(.3)
+                 ),
+                 child:Column(
+                   children: <Widget>[
+                     Slider(
+                         min:0,
+                         max:1000000,
+                         divisions: 10000,
+                         label: downPayment.round().toString(),
+                         onChanged: (DownP){
+                           setState((
+                               )  {
+                             downPayment = DownP;
+                           });
+                         },
+                         value: downPayment ),
+                     Container(
+                       margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                       child: Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: <Widget>[
+
+                             Text('Down Payment', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(1, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             ),
+                             Text('\$ ${double.parse(downPayment.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(2, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             )
+                           ],
+                         ),
+                       ),
+                     )
+                   ],
+
+                 ),
+               ),
+
                Padding(padding: EdgeInsets.all(20)),
                Container(
                  margin: const EdgeInsets.only(left: 30.0, right: 30.0),
@@ -234,17 +350,16 @@ class _HomePageState extends State<HomePage> {
                  child:Column(
                    children: <Widget>[
                      Slider(
-                         min:0,
-                         max:10000,
-                         divisions: 400,
-                         label: expenses.round().toString(),
-                         onChanged: (monthlyExpenses){
+                         min:0.5,
+                         max:5,
+                         label: propertyTax.round().toString(),
+                         onChanged: (pTax){
                            setState((
                                )  {
-                             expenses = monthlyExpenses;
+                             propertyTax = pTax;
                            });
                          },
-                         value: expenses ),
+                         value: propertyTax ),
                      Container(
                        margin: const EdgeInsets.only(left: 10.0, right: 10.0),
                        child: Padding(
@@ -253,7 +368,7 @@ class _HomePageState extends State<HomePage> {
                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                            children: <Widget>[
 
-                             Text('Monthly Expenses', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
+                             Text('Property Tax', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
                                  shadows: [
                                    Shadow(
                                        color: Colors.blue,
@@ -261,7 +376,60 @@ class _HomePageState extends State<HomePage> {
                                        blurRadius: 1),
                                  ]),
                              ),
-                             Text('\$ ${double.parse(expenses.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
+                             Text('${double.parse(propertyTax.toStringAsFixed(2))} %', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(2, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             )
+                           ],
+                         ),
+                       ),
+                     )
+                   ],
+
+                 ),
+               ),
+               Padding(padding: EdgeInsets.all(20)),
+               Container(
+                 margin: const EdgeInsets.only(left: 30.0, right: 30.0),
+                 decoration: BoxDecoration(
+                     border: Border.all(width:1,color: Colors.white),
+                     borderRadius: const BorderRadius.all(const Radius.circular(8)),
+                     color: Colors.white.withOpacity(.3)
+                 ),
+                 child:Column(
+                   children: <Widget>[
+                     Slider(
+                         min:0,
+                         max:3000,
+                         label: hoaTax.round().toString(),
+                         onChanged: (hoaT){
+                           setState((
+                               )  {
+                             hoaTax = hoaT;
+                           });
+                         },
+                         value: hoaTax ),
+                     Container(
+                       margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                       child: Padding(
+                         padding: const EdgeInsets.all(10.0),
+                         child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: <Widget>[
+
+                             Text('HOA', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 18,
+                                 shadows: [
+                                   Shadow(
+                                       color: Colors.blue,
+                                       offset: Offset(1, 2),
+                                       blurRadius: 1),
+                                 ]),
+                             ),
+                             Text('\$ ${double.parse(hoaTax.toStringAsFixed(2))}', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold, fontSize: 16,
                                  shadows: [
                                    Shadow(
                                        color: Colors.blue,
@@ -303,7 +471,8 @@ class _HomePageState extends State<HomePage> {
                          )
                      )
                  ),
-               )
+               ),
+               Padding(padding: EdgeInsets.all(20)),
 
              ],
 
